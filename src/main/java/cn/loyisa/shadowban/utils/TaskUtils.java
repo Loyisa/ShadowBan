0fc7f2a1dc01f8a342ec677fc46a44a738b2c63c
Loyisa <894857715@qq.com>
2022-01-22 19:39:13 +0800

    Create TaskUtils.java

---
+++ src/main/java/cn/loyisa/shadowban/utils/TaskUtils.java
@@ -0,0 +1,38 @@
+package cn.loyisa.shadowban.utils;
+
+import cn.loyisa.diona.Diona;
+import cn.loyisa.shadowban.ShadowBan;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
+public final class TaskUtils {
+
+    private static final ShadowBan shadowBan = ShadowBan.getInstance();
+
+    private TaskUtils() {
+    }
+
+    public static BukkitTask taskTimer(Runnable runnable, long delay, long interval) {
+        return Bukkit.getScheduler().runTaskTimer(shadowBan, runnable, delay, interval);
+    }
+
+    public static BukkitTask taskTimerAsync(Runnable runnable, long delay, long interval) {
+        return Bukkit.getScheduler().runTaskTimerAsynchronously(shadowBan, runnable, delay, interval);
+    }
+
+    public static BukkitTask task(Runnable runnable) {
+        return Bukkit.getScheduler().runTask(shadowBan, runnable);
+    }
+
+    public static BukkitTask taskAsync(Runnable runnable) {
+        return Bukkit.getScheduler().runTaskAsynchronously(shadowBan, runnable);
+    }
+
+    public static BukkitTask taskLater(Runnable runnable, long delay) {
+        return Bukkit.getScheduler().runTaskLater(shadowBan, runnable, delay);
+    }
+
+    public static BukkitTask taskLaterAsync(Runnable runnable, long delay) {
+        return Bukkit.getScheduler().runTaskLaterAsynchronously(shadowBan, runnable, delay);
+    }
+}
